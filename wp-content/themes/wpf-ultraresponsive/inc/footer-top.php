2afdfc208e8038f92ddcece2e9ad1cc0d90ca02a
mohammadq <ferozmohammad8041@gmail.com>
2015-09-25 13:30:27 +1000

    update1 by feroz
    study history

---
+++ wp-content/themes/wpf-ultraresponsive/inc/footer-top.php
@@ -0,0 +1,14 @@
+
+      <!-- Start footer top area -->
+      <div class="footer_top">
+        <div class="container">
+          <div class="row">
+		  
+
+			<?php dynamic_sidebar('footer-widgets'); ?>
+		  
+			
+          </div>
+        </div>
+      </div>
+      <!-- End footer top area -->
